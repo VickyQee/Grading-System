4f7af85a2771f0ee9cc35f12c5853ecba38fd36e
vickyQeeclear <chukwuv99@gmail.com>
2022-09-23 20:56:57 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+// This is a conditional expression that is used to print out students grades.
+
+let marks = (50)
+
+{
+if (marks >=90)
+console.log('Grade AA');
+else
+if (marks >80 && marks <=90)
+{
+console.log('Grade AB');
+}
+else
+if (marks >70 && marks <=80)
+{
+console.log('Grade BB');
+}
+else
+if (marks >60 && marks <=70)
+{
+console.log('Grade BC');
+}
+else
+if (marks >50 && marks <=60)
+{
+console.log('Grade CC');
+}
+else 
+if (marks >40 && marks <=50)
+{
+console.log('Grade CD');
+}
+else
+if (marks >30 && marks <=40)
+{
+console.log('Grade DD');
+}
+else
+console.log('Grade FF');
+}
